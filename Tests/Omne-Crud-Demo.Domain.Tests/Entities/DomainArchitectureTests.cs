7985b6a217466afee321c702c70c2a3f46ce9714
gmottajr <jrmottagerson@gmail.com>
2026-09-07 20:56:42 -0300

    feat(domain): Introducing architectural enforce unit tests for entities

---
+++ Tests/Omne-Crud-Demo.Domain.Tests/Entities/DomainArchitectureTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Omne_Crud_Demo.Abstractions;
+
+namespace Omne_Crud_Demo.Domain.Tests.Entities;
+
+public sealed class DomainArchitectureTests
+{
+    [Fact]
+    public void DomainEntities_ShouldInheritFromEntityBase()
+    {
+        var domainAssembly = typeof(Product).Assembly;
+
+        var entityTypes = domainAssembly
+            .GetTypes()
+            .Where(type =>
+                type.IsClass &&
+                !type.IsAbstract &&
+                typeof(EntityBase).IsAssignableFrom(type))
+            .ToList();
+
+        Assert.NotEmpty(entityTypes);
+
+        foreach (var type in entityTypes)
+        {
+            Assert.True(
+                typeof(EntityBase).IsAssignableFrom(type),
+                $"{type.Name} must inherit from {nameof(EntityBase)}.");
+        }
+    }
+}
